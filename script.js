0063d31b5f4619d65d82bc30bafc29a9ef26869d
Katja721 <katja.debevc72@gmail.com>
2022-06-23 12:36:03 +0200

    responsive design for mobile

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+const hamburgerIcon = document.querySelector(".hamburgerIcon");
+const closeIcon = document.querySelector(".closeIcon");
+const overlay = document.querySelector(".overlay");
+hamburgerIcon.addEventListener("click", function () {
+  overlay.classList.remove("hidden");
+  hamburgerIcon.classList.add("hidden");
+  closeIcon.classList.remove("hidden");
+});
+closeIcon.addEventListener("click", function () {
+  overlay.classList.add("hidden");
+  hamburgerIcon.classList.remove("hidden");
+  closeIcon.classList.add("hidden");
+});
